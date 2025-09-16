f93898bf201bab883b6da77aa9011406e71a03f7
phil-bstorm <philippe.haerens@bstorm.be>
2025-09-16 15:00:55 +0200

    [Reacto] Cars repository to use Models

--- repositories/cars.repository.js
+++ repositories/cars.repository.js
@@ -1,75 +1,105 @@
-let data = require("../mock/carsdatas.json");
-
+const {
+  models: { cars },
+} = require("../models/");
 
 //INTERNAL
 findNextId = () => {
-    const car = data?.at(-1);
-    if (!car) return 1;
-    else return user.id + 1;
-  };
-  //
-  
-  findAll = () => {
-    return new Promise((res, rej) => res(data));
-  };
-  
-  findFiltered = (filters) => {
-    
-    const predicate = []
-    if(filters.brand) predicate.push(c => c.brand.toLowerCase() == filters.brand.toLowerCase())
-    if(filters.model) predicate.push(c => c.model.toLowerCase() == filters.model.toLowerCase())
-    if(filters.minHp) predicate.push(c => c.hp>=filters.minHp)
-    if(filters.maxHp) predicate.push(c => c.hp<=filters.maxHp)
-    if(filters.minYear) predicate.push(c => c.year>=filters.minYear)
-    if(filters.maxYear) predicate.push(c => c.year<= filters.maxYear)
+  const car = data?.at(-1);
+  if (!car) return 1;
+  else return user.id + 1;
+};
+//
 
-    return new Promise((res, rej) => {
-      const result = data.filter(
-        c => predicate.every(p => p(c))
-      );
-      res(result);
-    });
-  };
-  
-  findById = (id) => {
-    return new Promise((res, rej) => {
-      const car = data.find((c) => c.id == id);
-      res(car);
-    });
-  };
-  
-  create = (car) => {
-    return new Promise((res, rej) => {
-      const nextId = findNextId();
-      car.id = nextId;
-      data.push(car);
-      res();
-    });
-  };
-  
-  update = (currentCar, car) => {
-      return new Promise((res, rej) => {
-          currentCar.model = car.model ?? currentCar.model;
-          currentCar.hp = car.hp ?? currentCar.hp;
-          currentCar.year = car.year ?? currentCar.year;
-          currentCar.brand = car.brand ?? currentCar.brand;
-          res();
-      })
+findAll = () => {
+  return cars.findAll();
+};
+
+findFiltered = (filters) => {
+  /*const predicate = [];
+  if (filters.brand)
+    predicate.push((c) => c.brand.toLowerCase() == filters.brand.toLowerCase());
+  if (filters.model)
+    predicate.push((c) => c.model.toLowerCase() == filters.model.toLowerCase());
+  if (filters.minHp) predicate.push((c) => c.hp >= filters.minHp);
+  if (filters.maxHp) predicate.push((c) => c.hp <= filters.maxHp);
+  if (filters.minYear) predicate.push((c) => c.year >= filters.minYear);
+  if (filters.maxYear) predicate.push((c) => c.year <= filters.maxYear);
+
+  return new Promise((res, rej) => {
+    const result = data.filter((c) => predicate.every((p) => p(c)));
+    res(result);
+  });*/
+
+  const where = {};
+  if (filters.brand) {
+    where.brand = filters.brand;
   }
-  
-  remove = (car) => {
-      return new Promise((res, rej) => {
-          data = data.filter(c => c.id != car.id)
-          res()
-      })
+  if (filters.model) {
+    where.model = filters.model;
   }
-  
-  module.exports = {
-    findAll,
-    findFiltered,
-    findById,
-    create,
-    update,
-    remove,
-  };
-  
+
+  if (filters.minHp || filters.maxHp) {
+    where.hp = {};
+    if (filters.maxHp) {
+      where.hp[Op.lte] = filters.maxHp;
+    }
+    if (filters.minHp) {
+      where.hp[Op.gte] = filters.minHp;
+    }
+  }
+
+  if (filters.minYear || filters.maxYear) {
+    where.year = {};
+    if (filters.maxYear) {
+      where.year[Op.lte] = filters.maxYear;
+    }
+    if (filters.minYear) {
+      where.year[Op.gte] = filters.minYear;
+    }
+  }
+
+  console.log(where);
+
+  // SELECT * FROM cars WHERE year >= 1999
+  return cars.findAll({
+    where,
+  });
+};
+
+findById = (id) => {
+  return cars.findByPk(id);
+};
+
+create = (car) => {
+  return cars.create(car);
+};
+
+update = (currentCar, car) => {
+  /*return new Promise((res, rej) => {
+    currentCar.model = car.model ?? currentCar.model;
+    currentCar.hp = car.hp ?? currentCar.hp;
+    currentCar.year = car.year ?? currentCar.year;
+    currentCar.brand = car.brand ?? currentCar.brand;
+    res();
+  });*/
+
+  currentCar.model = car.model ?? currentCar.model;
+  currentCar.hp = car.hp ?? currentCar.hp;
+  currentCar.year = car.year ?? currentCar.year;
+  if (car.brand !== undefined) currentCar.brand = car.brand; // authorise la valeur "null"
+
+  return currentCar.save();
+};
+
+remove = (car) => {
+  return car.destroy();
+};
+
+module.exports = {
+  findAll,
+  findFiltered,
+  findById,
+  create,
+  update,
+  remove,
+};
--- services/cars.services.js
+++ services/cars.services.js
@@ -1,66 +1,70 @@
-let data = require("../mock/carsdatas.json")
-const serviceCallResult = require("../responses/serviceCallResult.response")
-const carsRepository = require("../repositories/cars.repository")
+let data = require("../mock/carsdatas.json");
+const serviceCallResult = require("../responses/serviceCallResult.response");
+const carsRepository = require("../repositories/cars.repository");
 
-const findAll = async(filters) => {    
-    if(filters){
-        const result = await carsRepository.findFiltered(filters)
-        return serviceCallResult.ok(result)
-    }
- 
-    const result = await carsRepository.findAll()
-    return serviceCallResult.ok(result)
-}
+const findAll = async (filters) => {
+  console.log(Object.keys(filters));
+
+  if (Object.keys(filters).length) {
+    const result = await carsRepository.findFiltered(filters);
+    return serviceCallResult.ok(result);
+  }
+
+  const result = await carsRepository.findAll();
+  return serviceCallResult.ok(result);
+};
 
 const findOneById = async (id) => {
-    const car = await carsRepository.findById(id)
- 
-    if(!car) return serviceCallResult.notFound(`car with id #${id} not found`)
-    
-    return serviceCallResult.ok(car)
-}
+  const car = await carsRepository.findById(id);
+
+  if (!car) return serviceCallResult.notFound(`car with id #${id} not found`);
+
+  return serviceCallResult.ok(car);
+};
 
 const create = async (car = {}) => {
-    const {brand, model, year, hp} = car
+  const { brand, model, year, hp } = car;
 
-    if(brand && model && year && hp) {
-        await carsRepository.create(car);
-        return serviceCallResult.created()
-    } 
+  if (brand && model && year && hp) {
+    await carsRepository.create(car);
+    return serviceCallResult.created();
+  }
 
-    const errorDetails = ["A car must have "]
-    
-    if(!brand) errorDetails.push("a brand");
-    if(!model) errorDetails.push("a model");
-    if(!year) errorDetails.push("a year");
-    if(!hp) errorDetails.push("hps");
+  const errorDetails = ["A car must have "];
 
-    const error = errorDetails.join(" ,")
-    return serviceCallResult.badRequest(error)
-}
+  if (!brand) errorDetails.push("a brand");
+  if (!model) errorDetails.push("a model");
+  if (!year) errorDetails.push("a year");
+  if (!hp) errorDetails.push("hps");
+
+  const error = errorDetails.join(" ,");
+  return serviceCallResult.badRequest(error);
+};
 
 const update = async (id, newCar = {}) => {
-    const {brand, model, year, hp} = car
-     const car = await carsRepository.findById(id)
+  const car = await carsRepository.findById(id);
 
-     if(!car) return serviceCallResult.notFound(`car with id #${id} not found`)
-     
-     await carsRepository.update(car, newCar)
+  if (!car) return serviceCallResult.notFound(`car with id #${id} not found`);
 
-     return serviceCallResult.noContent()
+  await carsRepository.update(car, newCar);
 
-}
+  return serviceCallResult.noContent();
+};
 
 const remove = async (id) => {
-    const car = await carsRepository.findById(id)
- 
-    if(!car) return serviceCallResult.notFound(`car with id #${id} not found`)
-     
-     await carsRepository.remove(car)
- 
-     return serviceCallResult.noContent()
-}
+  const car = await carsRepository.findById(id);
+
+  if (!car) return serviceCallResult.notFound(`car with id #${id} not found`);
+
+  await carsRepository.remove(car);
+
+  return serviceCallResult.noContent();
+};
 
 module.exports = {
-    findAll, findOneById, create, update, remove
-}
+  findAll,
+  findOneById,
+  create,
+  update,
+  remove,
+};
